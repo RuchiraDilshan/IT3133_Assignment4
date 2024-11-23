9e15efca214b8f0231795f16af9cd551933f672b
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2024-11-23 23:00:32 +0530

    final

---
+++ assigment4/src/components/Game.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { animals } from '../data/AnimalsDb';
+
+export default function Game(){
+    const randomname = () => {
+        const index = Math.floor(Math.random() *animals.length);
+        return animals[index];
+
+    };
+
+    const [target, setTarget]=useState(randomname);
+    const [result,setResult]=useState('null');
+
+    const checker = (animal) => {
+        if (animal === target){
+
+            setResult('Win');
+
+        }
+        else{
+            setResult('Lost');
+
+        }
+    };
+
+    return(
+        <table width = "100%" style={{ textAligh:'center'}}>
+            <thead>
+                <tr>
+                    <th colspan='4'>Animal Game</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td wudth='20%'>{result}</td>
+                    <td width="20%">Animal Name</td>
+                    <td colSpan="2">Select Animal</td>
+                    
+                </tr>
+                <tr>
+                    <td width="20%">{result}</td>
+                    <td width="20%">{target.name}</td>
+                    <td colSpan="2">
+                        <div style={{display: 'flex', flexWrap: 'wrap', gap: '10px'}}>
+                            {animals.map((animal, index) => (
+                                <div
+                                    key={index}
+                                    onClick={() => checker(animal)}
+                                >
+                            <img
+                                src={require(`../assets/pictures/${animal.img}`)}
+                                alt={animal.name}
+                                style={{
+                                    width: '150px',
+                                    height: '150px',
+                                    objectFit: 'cover',
+                                    borderRadius: '5px',
+                                }}
+                                />
+                              </div>
+                            ))}
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
